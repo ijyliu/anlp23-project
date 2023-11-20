0865259428209a495063c132ce3260f4a838e81e
Isaac Liu <ijyliu@uchicago.edu>
2023-11-19 16:56:47 -0800

    new iteration

--- Data/Conversation_Functions.py
+++ Data/Conversation_Functions.py
@@ -87,29 +87,76 @@ def td3_gsm8k_tree_of_thought(question):
     tot_initial = "Task: Generate 3 different possible one-step calculations to serve as step 1 in solving the problem. Only work on step 1. Put each calculation on a new line. Do not number them."
     tot_prompt_2 = "Task: State the calculation above that is most likely to contribute to solving the problem. If it fully solves the original problem, also output STOP and the solution to the problem. If none of the calculations are correct, output ERROR and generate three new ones."
     tot_prompt_3 = "Task: Generate 3 different possible one-step calculations to serve as the next step in solving the problem. Only work on the next step. Put each calculation on a new line. Do not number them."
-    # Begin conversation
-    conversation = [formatted_question, tot_initial]
-    last_step = "initial"
+    # # Begin conversation
+    # conversation = [formatted_question, tot_initial]
+    # last_step_this_loop = ""
+    # # conversation = []
+    # # updated_convo = []
+    # new_last_step = "initial"
+    # # Maximum of 16 items in the conversation
+    # for _ in range(16):
+    #     # Create prompt
+    #     prompt = "\n".join(conversation)
+    #     # Get the response
+    #     response = davinci_completion(prompt)
+    #     conversation.append(response)
+    #     last_step_this_loop = new_last_step
+    #     # If the response contains STOP, stop
+    #     if "STOP" in response:
+    #         return conversation
+    #     # If last step was initial step or tot_prompt_3 need tot_prompt_2
+    #     # Also run this step if the response from tot_prompt_2 contains ERROR
+    #     if last_step_this_loop == "initial" or last_step_this_loop == "tot_prompt_3" or (last_step_this_loop == "tot_prompt_2" and "ERROR" in response):
+    #         conversation.append(tot_prompt_2)
+    #         new_last_step = "tot_prompt_2"
+    #     # If last step was tot_prompt_2 need tot_prompt_3
+    #     if last_step_this_loop == "tot_prompt_2":
+    #         conversation.append(tot_prompt_3)
+    #         new_last_step = "tot_prompt_3"
+    # Storing conversation elements
+    #conversation = []
+    updated_convo = []
+    new_last_step = ""
     # Maximum of 16 items in the conversation
-    for _ in range(16):
-        # Create prompt
-        prompt = "\n".join(conversation)
+    for i in range(16):
+        convo_to_feed = updated_convo.copy()
+        last_step_this_loop = new_last_step
         # Get the response
-        response = davinci_completion(prompt)
-        conversation.append(response)
+        if i == 0:
+            convo_to_feed.append(formatted_question)
+            convo_to_feed.append(tot_initial)
+            prompt = "\n".join(convo_to_feed)
+            response = davinci_completion(prompt)
+            updated_convo.append(response)
+            #updated_convo = prompt_gpt_4_and_get_convo(conversation, tot_initial)
+            print('initial response')
+            print(updated_convo)
+            new_last_step = "initial"
         # If the response contains STOP, stop
-        if "STOP" in response:
-            return conversation
+        if "STOP" in updated_convo[-1]:
+            print('STOPPING')
+            print(updated_convo)
+            return updated_convo
         # If last step was initial step or tot_prompt_3 need tot_prompt_2
         # Also run this step if the response from tot_prompt_2 contains ERROR
-        if last_step == "initial" or last_step == "tot_prompt_3" or (last_step == "tot_prompt_2" and "ERROR" in response):
-            conversation.append(tot_prompt_2)
-            last_step = "tot_prompt_2"
+        if last_step_this_loop == "initial" or last_step_this_loop == "tot_prompt_3" or (last_step_this_loop == "tot_prompt_2" and "ERROR" in updated_convo[-1]):
+            convo_to_feed.append(tot_prompt_2)
+            prompt = "\n".join(convo_to_feed)
+            response = davinci_completion(prompt)
+            updated_convo.append(response)
+            print('added tot2')
+            print(updated_convo)
+            new_last_step = "tot_prompt_2"
         # If last step was tot_prompt_2 need tot_prompt_3
-        elif last_step == "tot_prompt_2":
-            conversation.append(tot_prompt_3)
-            last_step = "tot_prompt_3"
-    return conversation
+        if last_step_this_loop == "tot_prompt_2":
+            convo_to_feed.append(tot_prompt_3)
+            prompt = "\n".join(convo_to_feed)
+            response = davinci_completion(prompt)
+            updated_convo.append(response)
+            print('added tot3')
+            print(updated_convo)
+            new_last_step = "tot_prompt_3"
+    return updated_convo
 
 # Self-Refine
 def td3_gsm8k_self_refine(question):
@@ -129,29 +176,41 @@ def td3_gsm8k_self_refine(question):
         # Get the response
         if i == 0:
             updated_convo.append(davinci_completion(formatted_question))
+            print('initial response')
+            print(updated_convo)
             new_last_step = "initial"
         #print(updated_convo)
         # If the response contains STOP, stop
         if "STOP" in updated_convo[-1]:
+            print('STOPPING')
+            print(updated_convo)
             return updated_convo
         # If last step was initial step or self_refine_3 need self_refine_2
         if last_step_this_loop == "initial" or last_step_this_loop == "self_refine_3":
             updated_convo.append(self_refine_2)
+            print('added srt2')
+            print(updated_convo)
             feeder_string = "\n".join(updated_convo)
             updated_convo.append(davinci_completion(feeder_string))
+            print('added response')
+            print(updated_convo)
             new_last_step = "self_refine_2"
         # If last step was self_refine_2 need self_refine_3
         if last_step_this_loop == "self_refine_2":
             updated_convo.append(self_refine_3)
+            print('added srt3')
+            print(updated_convo)
             feeder_string = "\n".join(updated_convo)
             updated_convo.append(davinci_completion(feeder_string))
+            print('added response')
+            print(updated_convo)
             new_last_step = "self_refine_3"
     return updated_convo
 
 # Least-to-Most
 def td3_gsm8k_least_to_most(question):
     # Format question and prompt
-    examples = "\nQ: Elsa has 5 apples. Anna has 2 more apples than Elsa. How many apples do they have together?\nA: Let's break down this problem: 1. How many apples does Anna have? 2. How many apples do Elsa and Anna have together?\n1. Anna has 2 more apples than Elsa. So Anna has 2 + 5 = 7 apples.2. Elsa and Anna have 5 + 7 = 12 apples together.\n"
+    examples = "\nQ: Elsa has 5 apples. Anna has 2 more apples than Elsa. How many apples do they have together?\nA: Let's break down this problem: 1. How many apples does Anna have? 2. How many apples do Elsa and Anna have together?\n1. Anna has 2 more apples than Elsa. So Anna has 2 + 5 = 7 apples. 2. Elsa and Anna have 5 + 7 = 12 apples together.\n"
     formatted_question = examples + "Q: " + question
     formatted_question = formatted_question + "\nA: Let's break down this problem:\n"
     # Storing conversation elements
@@ -264,36 +323,102 @@ def td3_cw_tree_of_thought(sentences):
     tot_prompt_4 = "Your Task: Select the most coherent draft that follows the rules of the task and write it out."
     tot_prompt_5 = "Your Task: If the draft is correct and coherent to the extent you would award a 10 on a scale of 1 to 10, output STOP. If it is not, write out a different one-sentence plan for a potential passage from among those considered and output PLAN."
     # Begin conversation
-    conversation = [task, tot_initial]
-    last_step = "initial"
+    # conversation = [task, tot_initial]
+    # last_step = ""
+    # new_last_step = "initial"
+    # # Maximum of 16 items in the conversation
+    # for _ in range(16):
+    #     # Create prompt
+    #     prompt = "\n".join(conversation)
+    #     # Get the response
+    #     response = davinci_completion(prompt)
+    #     conversation.append(response)
+    #     last_step = new_last_step
+    #     # If the response contains STOP, stop
+    #     if "STOP" in response or "Conversation truncated due to prompt length." in response:
+    #         return conversation
+    #     # If last step was initial step need tot_prompt_2
+    #     if last_step == "initial":
+    #         conversation.append(tot_prompt_2)
+    #         new_last_step = "tot_prompt_2"
+    #     # If last step was tot_prompt_2 need tot_prompt_3
+    #     # Also return to this step if the response from tot_prompt_5 contains PLAN
+    #     if last_step == "tot_prompt_2" or (last_step == "tot_prompt_5" and "PLAN" in response):
+    #         conversation.append(tot_prompt_3)
+    #         new_last_step = "tot_prompt_3"
+    #     # If last step was tot_prompt_3 need tot_prompt_4
+    #     if last_step == "tot_prompt_3":
+    #         conversation.append(tot_prompt_4)
+    #         new_last_step = "tot_prompt_4"
+    #     # If last step was tot_prompt_4 need tot_prompt_5
+    #     if last_step == "tot_prompt_4":
+    #         conversation.append(tot_prompt_5)
+    #         new_last_step = "tot_prompt_5"
+    # Begin conversation
+    conversation = []
+    initial_prompt = task + "\n" + tot_initial
+    last_step_this_loop = ""
+    new_last_step = ""
     # Maximum of 16 items in the conversation
-    for _ in range(16):
-        # Create prompt
-        prompt = "\n".join(conversation)
-        # Get the response
-        response = davinci_completion(prompt)
-        conversation.append(response)
+    for i in range(16):
+        last_step_this_loop = new_last_step
+        # Initial prompting
+        if i == 0:
+            # Get the response
+            #updated_convo = prompt_gpt_4_and_get_convo(conversation, initial_prompt)
+            conversation.append(initial_prompt)
+            prompt = "\n".join(conversation)
+            response = davinci_completion(prompt)
+            updated_convo = conversation.copy()
+            updated_convo.append(response)
+            new_last_step = "initial"
         # If the response contains STOP, stop
-        if "STOP" in response or "Conversation truncated due to prompt length." in response:
-            return conversation
-        # If last step was initial step of tot_prompt_3 need tot_prompt_2
-        if last_step == "initial" or last_step == "tot_prompt_3":
-            conversation.append(tot_prompt_2)
-            last_step = "tot_prompt_2"
+        if "STOP" in updated_convo[-1]:
+            print('STOPPING')
+            print(updated_convo)
+            return updated_convo
+        # If last step was initial step need tot_prompt_2
+        if last_step_this_loop == "initial":
+            updated_convo.append(tot_prompt_2)
+            prompt = "\n".join(updated_convo)
+            response = davinci_completion(prompt)
+            updated_convo.append(response)
+            print('added tot2')
+            print(updated_convo)
+            new_last_step = "tot_prompt_2"
         # If last step was tot_prompt_2 need tot_prompt_3
         # Also return to this step if the response from tot_prompt_5 contains PLAN
-        elif last_step == "tot_prompt_2" or (last_step == "tot_prompt_5" and "PLAN" in response):
-            conversation.append(tot_prompt_3)
-            last_step = "tot_prompt_3"
+        if last_step_this_loop == "tot_prompt_2" or (last_step_this_loop == "tot_prompt_5" and "PLAN" in updated_convo[-1]):
+            #updated_convo = prompt_gpt_4_and_get_convo(updated_convo, tot_prompt_3)
+            updated_convo.append(tot_prompt_3)
+            prompt = "\n".join(updated_convo)
+            response = davinci_completion(prompt)
+            updated_convo.append(response)
+            print('added tot3')
+            print(updated_convo)
+            new_last_step = "tot_prompt_3"
         # If last step was tot_prompt_3 need tot_prompt_4
-        elif last_step == "tot_prompt_3":
-            conversation.append(tot_prompt_4)
-            last_step = "tot_prompt_4"
+        if last_step_this_loop == "tot_prompt_3":
+            #updated_convo = prompt_gpt_4_and_get_convo(updated_convo, tot_prompt_4)
+            updated_convo.append(tot_prompt_4)
+            prompt = "\n".join(updated_convo)
+            response = davinci_completion(prompt)
+            updated_convo.append(response)
+            print('added tot4')
+            print(updated_convo)
+            new_last_step = "tot_prompt_4"
         # If last step was tot_prompt_4 need tot_prompt_5
-        elif last_step == "tot_prompt_4":
-            conversation.append(tot_prompt_5)
-            last_step = "tot_prompt_5"
-    return conversation
+        if last_step_this_loop == "tot_prompt_4":
+            #updated_convo = prompt_gpt_4_and_get_convo(updated_convo, tot_prompt_5)
+            updated_convo.append(tot_prompt_5)
+            prompt = "\n".join(updated_convo)
+            response = davinci_completion(prompt)
+            updated_convo.append(response)
+            print('added tot5')
+            print(updated_convo)
+            new_last_step = "tot_prompt_5"
+    print(updated_convo)
+    return updated_convo
 
 # Self-Refine
 def td3_cw_self_refine(sentences):
@@ -464,18 +589,26 @@ def gpt4_gsm8k_tree_of_thought(question):
         # Get the response
         if i == 0:
             updated_convo = prompt_gpt_4_and_get_convo(conversation, tot_initial)
+            print('initial response')
+            print(updated_convo)
             new_last_step = "initial"
         # If the response contains STOP, stop
         if "STOP" in updated_convo[-1]['content']:
+            print('STOPPING')
+            print(updated_convo)
             return updated_convo
         # If last step was initial step or tot_prompt_3 need tot_prompt_2
         # Also run this step if the response from tot_prompt_2 contains ERROR
         if last_step_this_loop == "initial" or last_step_this_loop == "tot_prompt_3" or (last_step_this_loop == "tot_prompt_2" and "ERROR" in updated_convo[-1]['content']):
             updated_convo = prompt_gpt_4_and_get_convo(convo_to_feed, tot_prompt_2)
+            print('added tot2')
+            print(updated_convo)
             new_last_step = "tot_prompt_2"
         # If last step was tot_prompt_2 need tot_prompt_3
         if last_step_this_loop == "tot_prompt_2":
             updated_convo = prompt_gpt_4_and_get_convo(convo_to_feed, tot_prompt_3)
+            print('added tot3')
+            print(updated_convo)
             new_last_step = "tot_prompt_3"
     return updated_convo
 
@@ -630,32 +763,48 @@ def gpt4_cw_tree_of_thought(sentences):
     tot_prompt_5 = "Your Task: If the draft is correct and coherent to the extent you would award a 10 on a scale of 1 to 10, output STOP. If it is not, write out a different one-sentence plan for a potential passage from among those considered and output PLAN."
     # Begin conversation
     conversation = []
+    updated_convo = []
     initial_prompt = task + "\n" + tot_initial
-    last_step = "initial"
+    last_step = ""
+    new_last_step = ""
     # Maximum of 16 items in the conversation
-    for _ in range(16):
-        # Get the response
-        updated_convo = prompt_gpt_4_and_get_convo(conversation, initial_prompt)
+    for i in range(16):
+        last_step = new_last_step
+        # Initial prompting
+        if i == 0:
+            # Get the response
+            updated_convo = prompt_gpt_4_and_get_convo(conversation, initial_prompt)
+            new_last_step = "initial"
         # If the response contains STOP, stop
         if "STOP" in updated_convo[-1]['content']:
+            print('STOPPING')
+            print(updated_convo)
             return updated_convo
-        # If last step was initial step of tot_prompt_3 need tot_prompt_2
-        if last_step == "initial" or last_step == "tot_prompt_3":
+        # If last step was initial step need tot_prompt_2
+        if last_step == "initial":
             updated_convo = prompt_gpt_4_and_get_convo(updated_convo, tot_prompt_2)
-            last_step = "tot_prompt_2"
+            print('added tot2')
+            print(updated_convo)
+            new_last_step = "tot_prompt_2"
         # If last step was tot_prompt_2 need tot_prompt_3
         # Also return to this step if the response from tot_prompt_5 contains PLAN
-        elif last_step == "tot_prompt_2" or (last_step == "tot_prompt_5" and "PLAN" in updated_convo[-1]['content']):
+        if last_step == "tot_prompt_2" or (last_step == "tot_prompt_5" and "PLAN" in updated_convo[-1]['content']):
             updated_convo = prompt_gpt_4_and_get_convo(updated_convo, tot_prompt_3)
-            last_step = "tot_prompt_3"
+            print('added tot3')
+            print(updated_convo)
+            new_last_step = "tot_prompt_3"
         # If last step was tot_prompt_3 need tot_prompt_4
-        elif last_step == "tot_prompt_3":
+        if last_step == "tot_prompt_3":
             updated_convo = prompt_gpt_4_and_get_convo(updated_convo, tot_prompt_4)
-            last_step = "tot_prompt_4"
+            print('added tot4')
+            print(updated_convo)
+            new_last_step = "tot_prompt_4"
         # If last step was tot_prompt_4 need tot_prompt_5
-        elif last_step == "tot_prompt_4":
+        if last_step == "tot_prompt_4":
             updated_convo = prompt_gpt_4_and_get_convo(updated_convo, tot_prompt_5)
-            last_step = "tot_prompt_5"
+            print('added tot5')
+            print(updated_convo)
+            new_last_step = "tot_prompt_5"
     print(updated_convo)
     return updated_convo
 
