de12763029bc7ed21f84b249d93b449dba599002
Isaac Liu <ijyliu@uchicago.edu>
2023-11-27 23:05:50 -0800

    compliance bugfixes up through gpt4 dp convo 14

--- Data/Evaluation_Functions.py
+++ Data/Evaluation_Functions.py
@@ -1,9 +1,10 @@
 
 # Functions useful to evaluate conversations
 
-import tiktoken
+import re
 import nltk
 import numpy as np
+from unidecode import unidecode
 
 # Strip GPT-4 related metadata from a conversation
 def strip_gpt4_meta(conversation):
@@ -23,9 +24,10 @@ def strip_gpt4_meta(conversation):
         no_meta.append(stripped)
     return no_meta
 
+# Cleanup for CW compliance
 # Strip GPT-4 related metadata from a conversation
 # DO NOT remove extra whitespace in this version
-def strip_gpt4_meta_no_ew_removal(conversation):
+def gpt4_cw_cleanup(conversation):
     # Strip some material from each line
     # Get rid of "{'role': 'user', 'content': "
     # Get rid of "{'role': 'assistant', 'content': "
@@ -37,10 +39,27 @@ def strip_gpt4_meta_no_ew_removal(conversation):
         stripped = message_and_metadata.replace("{'role': 'user', 'content': ", "")
         stripped = stripped.replace("{'role': 'assistant', 'content': ", "")
         stripped = stripped.replace("}", "")
+        # Replace any number of spaces and then a newline with a newline
+        stripped = re.sub(r'\s+\n', '\n', stripped)
+        # Convert to ascii
+        stripped = unidecode(stripped)
         #stripped = stripped.strip()
         stripped = stripped[1:-1]
+        # Strip newlines or spaces at the very end of the string
+        stripped = re.sub(r'[\s\n]+$', '', stripped)
         no_meta.append(stripped)
     return no_meta
+
+# Text-davinci-003 cleanup
+def td3_cw_cleanup(conversation):
+    cleaned_up = []
+    for message in conversation:
+        # Remove any number of spaces and then a newline
+        new_message = re.sub(r'\s+\n', '\n', message)
+        # Convert to ascii
+        new_message = unidecode(new_message)
+        cleaned_up.append(new_message)
+    return cleaned_up
 
 # Get length using the tokenizer for the appropriate model
 def get_length(string, model):
